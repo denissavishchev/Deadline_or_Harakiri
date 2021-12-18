431fa56ecd02b9e77077b387dd7771d54ee786cd
Denis Savishchev <denis.savishchev@gmail.com>
2021-12-18 14:33:38 +0100

    color_theme

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ class Harakiri(MDApp, Screen):
     def show_task_dialog(self):
         if not self.task_list_dialog:
             self.task_list_dialog = MDDialog(
-                title="Create Task",
+                title="[color=008F11]Create Task[/color]",
                 type="custom",
                 content_cls=DialogContent(),
             )
@@ -167,7 +167,75 @@ class Harakiri(MDApp, Screen):
 
 
     def build(self):
-        self.theme_cls.primary_palette = "DeepOrange"
+        colors = {
+            "Teal": {
+                "50": "e4f8f9",
+                "100": "bdedf0",
+                "200": "97e2e8",
+                "300": "79d5de",
+                "400": "6dcbd6",
+                "500": "008F11",#reference
+                "600": "63b2bc",
+                "700": "5b9ca3",
+                "800": "54888c",
+                "900": "486363",
+                "A100": "bdedf0",
+                "A200": "97e2e8",
+                "A400": "6dcbd6",
+                "A700": "5b9ca3",
+            },
+            "Blue": {
+                "50": "e3f3f8",
+                "100": "b9e1ee",
+                "200": "91cee3",
+                "300": "72bad6",
+                "400": "62acce",
+                "500": "589fc6",
+                "600": "5191b8",
+                "700": "487fa5",
+                "800": "426f91",
+                "900": "35506d",
+                "A100": "b9e1ee",
+                "A200": "91cee3",
+                "A400": "62acce",
+                "A700": "487fa5",
+            },
+            "Red": {
+                "50": "ff6600",
+                "100": "ff6600",
+                "200": "EF9A9A",
+                "300": "E57373",
+                "400": "EF5350",
+                "500": "ff6600",
+                "600": "E53935",
+                "700": "D32F2F",
+                "800": "C62828",
+                "900": "B71C1C",
+                "A100": "FF8A80",
+                "A200": "FF5252",
+                "A400": "FF1744",
+                "A700": "D50000",
+            },
+            "Light": {
+                "StatusBar": "E0E0E0",
+                "AppBar": "F5F5F5",
+                "Background": "FAFAFA",
+                "CardsDialogs": "FFFFFF",
+                "FlatButtonDown": "cccccc",
+            },
+            "Dark": {
+                "StatusBar": "000000",
+                "AppBar": "212121",
+                "Background": "303030",
+                "CardsDialogs": "424242",
+                "FlatButtonDown": "999999",
+            }
+        }
+
+        self.theme_cls.colors = colors
+        self.theme_cls.primary_palette = "Teal"
+        self.theme_cls.theme_style = "Dark"
+        # self.theme_cls.primary_palette = "Green" #Teal, LightGreen, Lime
 
         global screen_manager
         screen_manager = ScreenManager()
@@ -233,7 +301,7 @@ class Harakiri(MDApp, Screen):
             # minutes = str((deadline.seconds % 3600) // 60)
             # seconds = str((deadline.seconds % 3600) % 60)
 
-            date_cooldown = (f'Days: {deadline.days}[color=#808080][size=24]({full_days.days})[/size][/color]')#, {hours}:{minutes}:{seconds}
+            date_cooldown = (f'Days: {deadline.days}[color=#3d3d3d][size=24]({full_days.days})[/size][/color]')#, {hours}:{minutes}:{seconds}
 
             progress = (deadline.days / full_days.days) * 100
 
